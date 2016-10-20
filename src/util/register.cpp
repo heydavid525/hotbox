c78e9e804c429cd27cd4cb6289c2aae8f431b1f5
Dai Wei <daiwei89@gmail.com>
2016-10-19 22:32:21 -0600

    TF builds correctly now

--- src/util/register.cpp
+++ src/util/register.cpp
@@ -56,7 +56,9 @@ void RegisterTransforms() {
       Creator<TransformIf, TfTransform>);
   kConfigCaseToTransformName[TransformConfig::kTfTransform]
     = "TfTransform";
+  LOG(INFO) << "Register TfTransform";
 #endif
+  LOG(INFO) << "Registering Kmeans transform";
   registry.AddCreator(TransformConfig::kKmeansTransform,
       Creator<TransformIf, KmeansTransform>);
   kConfigCaseToTransformName[TransformConfig::kKmeansTransform]
